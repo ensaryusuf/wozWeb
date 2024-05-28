3779b3f55289aa12b390d0fe60cedf6aa44e077a
Ensar Yusuf Yılmaz <ensaryusufyilmaz@gmail.com>
2024-05-28 19:55:10 +0300

    v2

--- sliderjs.php
+++ sliderjs.php
@@ -1,4 +1,4 @@
-{"slider":["http:\/\/rest.monkapp.site\/pages\/sliderres\/photo1707134961.jpeg"]}}
+{"slider":["http:\/\/rest.monkapp.site\/pages\/sliderres\/Football%20Tricks%20To%20Lit%20You%20UP!.png"]}}
 
 
 ?>
